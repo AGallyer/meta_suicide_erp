40ba19bd18b8c033ba1adb72bc859912469f831e
Austin Gallyer <ajg17d@fsu.edu>
2021-09-08 10:39:47 -0400

    Final commit

--- meta_power_analysis.R
+++
@@ -1,8 +0,0 @@
-library(dmetar)
-library(compute.es)
-power.analysis(d = 0.24, 
-                k = 3, 
-                n1 = 30, 
-                n2 = 30, 
-                p = 0.05, 
-               heterogeneity = "low")
